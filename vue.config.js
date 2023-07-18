dd32f14793e2f89bd51404a6ee7e355a47782c60
wxl <1715142548@qq.com>
2023-07-18 14:42:40 +0800

    修改监听地址，使所有地址均可访问

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 80
     // proxy: {
     //   '/gateway': {
