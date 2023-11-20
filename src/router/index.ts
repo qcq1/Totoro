5d1cc205f90d77cb76a712a9480a94f032caa22e
wxl <1715142548@qq.com>
2023-11-20 17:27:55 +0800

    添加首页标题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,27 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import App from '@/App.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'main',
+    component: App,
+    meta: {
+      title: '星辉万物'
+    }
+  }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+router.beforeEach(async (to) => {
+  if (to.meta.title) { // 判断是否有标题
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    document.title = to.meta.title
+  }
 })
 
 export default router
